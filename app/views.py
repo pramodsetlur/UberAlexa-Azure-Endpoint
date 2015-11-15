49ef4ec104db7353249cf8522bd0576ab1f98319
Pramod Setlur <pramod.setlur@ticketmaster.com>
2015-11-14 23:26:24 -0800

    Adding Project

---
+++ app/views.py
@@ -0,0 +1,29 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+YOUR_INFO = {
+    'name' : 'Your name',
+    'bio' : 'What\'s your deal? What do you do?',
+    'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
+    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
+    'github_username' : "fetchpush", 
+    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+}
+    
+def home(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/base.html',
+        context_instance = RequestContext(request,
+            {
+                'attendee' : PRAMOD,    
+                'year': datetime.now().year,
+            })
+    )
